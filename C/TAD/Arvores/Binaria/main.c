ced921fc3b36c04c1f97b4e4f7e68501892488ab
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2017-03-19 22:05:39 -0300

    Arvore Binaria de Busca completa com todas as operações

--- C/TAD/Arvores/Binaria/main.c
+++ C/TAD/Arvores/Binaria/main.c
@@ -21,7 +21,9 @@ void buscaRecursiva();
 void buscaIterativa();
 void insercaoIterativa();
 void insercaoRecursiva();
-void remocao();
+No *maisDireita();
+No *maisEsquerda();
+No *remocao();
 void percursoPre();
 void percursoIn();
 void percursoPos();
@@ -77,9 +79,33 @@ int main(){
 
 				insercaoRecursiva(Arvore,valorInserido);		
 				break;
+
+			case 5:
+			;
+				printf("Digite o valor para Remocao\n");	
+				scanf("%d",&valorInserido);
+
+				remocao(Arvore,valorInserido);
+				break;
+
+			case 6:
+			;
+				percursoPre(Arvore);
+				break;
+
+			case 7:
+			;
+				percursoIn(Arvore);
+				break;
+
+			case 8:
+			;
+				percursoPos(Arvore);
+				break;
 				
 			case 9:
 			;
+				free(Arvore);
 				sair = true;
 				break;
 
@@ -98,6 +124,23 @@ int main(){
 
 	return 0;
 }
+
+void imprimirOpcoes(){
+	printf("--------------------- Arvore Binaria de Busca ---------------------\n");
+	printf("0-Mostrar essas opcoes novamente\n");
+	printf("1-Busca Recursiva\n");
+	printf("2-Busca Iterativa\n");
+	printf("3-Insercao Iterativa\n");
+	printf("4-Insercao Recursiva\n");	
+	printf("5-Remocao\n");
+	printf("6-Percurso pre-ordem\n");
+	printf("7-Percurso in-ordem\n");
+	printf("8-Percurso pos-ordem\n");
+	printf("-------------------------------------------------------------------\n");
+	printf("11 - Checa o status do ultimo nó\n");
+	printf("9 - Sair/Fechar o programa\n\n");
+}
+
 void buscaIterativa(No **paiRaiz, int valorInserido){
 	No *copiaPaiRaiz = *paiRaiz;
 
@@ -131,55 +174,38 @@ void buscaRecursiva(No **paiRaiz, int valorInserido){
 	}
 }
 
-void imprimirOpcoes(){
-	printf("--------------------- Arvore Binaria de Busca ---------------------\n");
-	printf("0-Mostrar essas opcoes novamente\n");
-	printf("1-Busca Recursiva\n");
-	printf("2-Busca Iterativa\n");
-	printf("3-Insercao Iterativa\n");
-	printf("4-Insercao Recursiva\n");	
-	printf("5-Remocao\n");
-	printf("6-Percurso pre-ordem\n");
-	printf("7-Percurso in-ordem\n");
-	printf("8-Percurso pos-ordem\n");
-	printf("-------------------------------------------------------------------\n");
-	printf("11 - Checa o status do ultimo nó\n");
-	printf("9 - Sair/Fechar o programa\n\n");
-}
-
 void insercaoIterativa(No **paiRaiz, int valorInserido){
-	No *pai;
+	No *p,*q;
 
 	if(*paiRaiz == NULL){
-		*paiRaiz = (No*) malloc(sizeof(No));
-		(*paiRaiz)->esq = NULL;
-		(*paiRaiz)->dir = NULL;
-		(*paiRaiz)->numero = valorInserido;
+		p = (No*) malloc(sizeof(No));
+		p->esq = NULL;
+		p->dir = NULL;
+		p->numero = valorInserido;
+		(*paiRaiz) = p;
 	}
+	else{
+		p = (*paiRaiz);
 
-	while((*paiRaiz) != NULL && (*paiRaiz)->numero != valorInserido){
-		pai = (*paiRaiz);
+		while(p != NULL){
+			if(p->numero > valorInserido){
+				q = p;
+				p = p->esq;
+			}
+			else{
+				q = p;
+				p = p->dir;
+			}
+		}
+		p = (No*) malloc(sizeof(No));
+		p->esq = NULL;
+		p->dir = NULL;
+		p->numero = valorInserido;
 		
-		if(valorInserido < (*paiRaiz)->numero) (*paiRaiz) = (*paiRaiz)->esq;
-
-		else if(valorInserido > (*paiRaiz)->numero) (*paiRaiz) = (*paiRaiz)->dir;
-
-	};
-	
-	if(*paiRaiz == NULL){
-		*paiRaiz = (No*) malloc(sizeof(No));
-		(*paiRaiz)->esq = NULL;
-		(*paiRaiz)->dir = NULL;
-		(*paiRaiz)->numero = valorInserido;
-		
-		if(valorInserido < pai->numero) pai->esq = (*paiRaiz);
-
-		else pai->dir = (*paiRaiz);	
-
-		(*paiRaiz) = pai;
+		if(q->numero > valorInserido) q->esq = p;
+		else q->dir = p;
 	}
 }
-
 
 void insercaoRecursiva(No **paiRaiz, int valorInserido){
 	if(*paiRaiz == NULL){
@@ -194,6 +220,74 @@ void insercaoRecursiva(No **paiRaiz, int valorInserido){
 		else if(valorInserido > (*paiRaiz)->numero) insercaoRecursiva(&(*paiRaiz)->dir, valorInserido);
 
 		else printf("O valor já existe!\n");
+	}
+}
+
+No *maisDireita(No **paiRaiz){
+	while((*paiRaiz)->dir !=NULL){
+		(*paiRaiz) = (*paiRaiz)->dir;
+
+		return (*paiRaiz);
+	}
+}
+
+No *maisEsquerda(No **paiRaiz){
+	while((*paiRaiz)->esq !=NULL){
+		(*paiRaiz) = (*paiRaiz)->esq;
+
+		return (*paiRaiz);
+	}
+}
+
+No *remocao(No **paiRaiz, int valorInserido){
+	if(*paiRaiz == NULL) printf("O valor %d não existe na Arvore\n", valorInserido);
+	else{
+		if(valorInserido < (*paiRaiz)->numero) remocao(&(*paiRaiz)->esq,valorInserido);
+		else if(valorInserido > (*paiRaiz)->numero) remocao(&(*paiRaiz)->dir,valorInserido);
+		
+		else{
+			if((*paiRaiz)->esq != NULL){
+				No *auxiliar = maisDireita(&(*paiRaiz)->esq);
+				(*paiRaiz)->numero = auxiliar->numero;
+
+				(*paiRaiz)->esq = remocao(&(*paiRaiz)->esq, auxiliar->numero);
+			}
+			else if((*paiRaiz)->dir != NULL){
+				No *auxiliar = maisEsquerda(&(*paiRaiz)->dir);
+				(*paiRaiz)->numero = auxiliar->numero;
+
+				(*paiRaiz)->dir = remocao(&(*paiRaiz)->dir, auxiliar->numero);
+			}
+			else{
+				printf("O valor %d foi removido!\n", (*paiRaiz)->numero);
+				(*paiRaiz) = NULL;
+			}			
+		}
+	}
+	return (*paiRaiz);
+}
+
+void percursoPre(No **paiRaiz){
+	if((*paiRaiz) != NULL){
+		printf("%d ", (*paiRaiz)->numero);
+		percursoPre(&(*paiRaiz)->esq);
+		percursoPre(&(*paiRaiz)->dir);
+	}	
+}
+
+void percursoIn(No **paiRaiz){
+	if((*paiRaiz) != NULL){
+		percursoIn(&(*paiRaiz)->esq);
+		printf("%d ", (*paiRaiz)->numero);
+		percursoIn(&(*paiRaiz)->dir);
+	}
+}
+
+void percursoPos(No **paiRaiz){
+	if((*paiRaiz) != NULL){
+		percursoPos(&(*paiRaiz)->esq);		
+		percursoPos(&(*paiRaiz)->dir);
+		printf("%d ", (*paiRaiz)->numero);
 	}
 }
 
